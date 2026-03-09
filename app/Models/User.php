142e99647501123bfe372466a847c943dc4a8159
anuththara2007-W <you@example.com>
2026-03-09 10:13:48 +0530

    feat: add profile update functionality including optional avatar update

--- app/Models/User.php
+++ app/Models/User.php
@@ -67,4 +67,28 @@ class User
         return false;
     }
 
+    //feat: add profile update functionality including optional avatar update
+    
+    public function updateProfile($userId, $data)
+    {
+        $query = "UPDATE users SET full_name = :full_name, phone = :phone";
+        $params = [
+            'full_name' => $data['full_name'],
+            'phone' => $data['phone'],
+            'user_id' => $userId
+        ];
+
+        if (array_key_exists('profile_image', $data)) {
+            $query .= ", profile_image = :profile_image";
+            $params['profile_image'] = $data['profile_image'];
+        }
+
+        $query .= " WHERE user_id = :user_id";
+        
+        $stmt = $this->db->prepare($query);
+        return $stmt->execute($params);
+    }
+
+    
+
 }
