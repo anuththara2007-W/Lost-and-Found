ac93f1ba4f5e98a220e4860eaad447ff8629658b
anuththara2007-W <you@example.com>
2026-03-09 09:24:55 +0530

    initialize UserController with User and Item models and enforce login

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -5,5 +5,14 @@ use App\Core\Controller;
 
 class UserController extends Controller
 {
-    
+     private $userModel;
+    private $itemModel;
+
+    public function __construct()
+    {
+        requireLogin();
+        $this->userModel = $this->model('User');
+        $this->itemModel = $this->model('Item');
+    }
+
 }
