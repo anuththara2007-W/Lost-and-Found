7ed5c6c40ecf14713741bd2a7352d7a4e800bdbf
pabasara madhawa <pabasaramadhawa820@gmail.com>
2026-03-17 11:16:52 +0530

    add resources

---
+++ resources/views/messages/chat.php
@@ -0,0 +1,2 @@
+<?php require_once ROOT . '/resources/views/layouts/header.php'; ?>
+<link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/messages/chat-conversation.css">//add resourcebundle_locales
