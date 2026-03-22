bf916c03bd3a1516d939c13f2e8f1f6af2241c37
anuththara2007-W <anuththarawickramasekara424@gmail.com>
2026-03-22 10:44:09 +0530

    MapController -  construct done

--- app/Controllers/MapController.php
+++ app/Controllers/MapController.php
@@ -5,4 +5,11 @@ use App\Core\Controller;
 
 class MapController extends Controller
 {
+
+ private $itemModel;
+
+    public function __construct()
+    {
+        $this->itemModel = $this->model('Item');
+    }
 }
