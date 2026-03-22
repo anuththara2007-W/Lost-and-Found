fafd430bd87783f3a4e06a1e0b72d16400dd8b64
anuththara2007-W <anuththarawickramasekara424@gmail.com>
2026-03-22 09:39:50 +0530

    NotificationService.php - Send to mobile

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -8,5 +8,9 @@ class NotificationService
         error_log("NotificationService: Sending EMAIL to $to | Subject: $subject");
         return true;
     }
-
+public static function sendSMS($phoneNumber, $message)
+    {
+        error_log("NotificationService: Sending SMS to $phoneNumber | Message: $message");
+        return true;
+    }
 }
