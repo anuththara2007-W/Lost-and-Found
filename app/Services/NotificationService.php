1d9b63013e906c61ccca77ba2d177f014d99c648
anuththara2007-W <anuththarawickramasekara424@gmail.com>
2026-03-22 09:39:13 +0530

    NotificationService.php - Send to Email

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -2,5 +2,11 @@
 namespace App\Services;
 class NotificationService
 {
+ public static function sendEmail($to, $subject, $body)
+    {
+       
+        error_log("NotificationService: Sending EMAIL to $to | Subject: $subject");
+        return true;
+    }
 
 }
