2b396c6182ae8fbb8977986e3382b7805afbaa83
Pawan Tharusha <pawantharusha7002@gmail.com>
2026-04-23 09:38:10 +0530

    create.php - Add item fileds

--- resources/views/items/create.php
+++ resources/views/items/create.php
@@ -11,3 +11,24 @@
             <p>Thank you for helping! Let's get this back to its rightful owner.</p>
         <?php endif; ?>
     </div>
+
+     <form action="<?= BASE_URL ?>/item/create" method="POST" enctype="multipart/form-data">
+        <input type="hidden" name="type" value="<?= escape($type) ?>">
+
+        <div class="input-group">
+            <label class="input-label" for="title">Item Name (Short Reference)</label>
+            <input type="text" id="title" name="title" class="input-field" placeholder="e.g. Blue Hydro Flask" required value="<?= old('title') ?>">
+        </div>
+
+        <div class="input-group">
+            <label class="input-label" for="category_id">Category</label>
+            <select id="category_id" name="category_id" class="input-field" required onchange="toggleCustomCategory()">
+                <option value="">-- Select Category --</option>
+                <?php foreach($categories as $cat): ?>
+                    <option value="<?= $cat['category_id'] ?>" <?= old('category_id') == $cat['category_id'] ? 'selected' : '' ?>>
+                        <?= escape($cat['name']) ?>
+                    </option>
+                <?php endforeach; ?>
+                <option value="other" <?= old('category_id') == 'other' ? 'selected' : '' ?>>Other (Specify Custom Category)</option>
+            </select>
+        </div>
