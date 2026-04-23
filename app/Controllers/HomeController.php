749293a81d6fb011b3879ccace1fafb8aefde3d9
anuththara2007-W <anuththarawickramasekara424@gmail.com>
2026-04-23 10:55:40 +0530

    HomeController.php - fixed

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,14 +1,49 @@
 <?php
 namespace App\Controllers;
 
-class HomeController
+use App\Core\Controller;
+
+class HomeController extends Controller
 {
+    private $itemModel;
+
+    public function __construct()
+    {
+        // Try not to fail if the Item model isn't built yet
+        if(file_exists(ROOT . '/app/Models/Item.php')) {
+            $this->itemModel = $this->model('Item');
+        }
+    }
+
     public function index()
     {
-        // Example data (important because your view expects it)
+        // Get recent items
         $recentItems = [];
+        if($this->itemModel) {
+            $recentItems = $this->itemModel->getRecentReports(6);
+        }
 
-        // Load the view
-        require_once ROOT . '/resources/views/home.php';
+        $data = [
+            'title' => 'Lost & Found - Home',
+            'recentItems' => $recentItems
+        ];
+
+        $this->view('home', $data);
+    }
+
+    public function success_stories()
+    {
+        $resolvedItems = [];
+        if($this->itemModel) {
+            $resolvedItems = $this->itemModel->getResolvedReports(20);
+        }
+
+        $data = [
+            'title' => 'Success Stories & Rewards - Lost and Found',
+            'resolvedItems' => $resolvedItems,
+            'css' => ['success']
+        ];
+
+        $this->view('success_stories', $data);
     }
 }
