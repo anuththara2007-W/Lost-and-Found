9fab383328de3c5ba49bb3f167b57fecff2b24a1
anuththara2007-W <anuththarawickramasekara424@gmail.com>
2026-04-23 17:14:57 +0530

    user.php - find by funtions

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,4 +13,24 @@ class User
     {
         $this->db = Database::getInstance()->getConnection();
     }
+        public function findByEmail($email)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM users WHERE email = :email");
+        $stmt->execute(['email' => $email]);
+        return $stmt->fetch();
+    }
+
+    public function findByUsername($username)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM users WHERE username = :username");
+        $stmt->execute(['username' => $username]);
+        return $stmt->fetch();
+    }
+
+    public function findById($id)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM users WHERE user_id = :id");
+        $stmt->execute(['id' => $id]);
+        return $stmt->fetch();
+    }
 }
