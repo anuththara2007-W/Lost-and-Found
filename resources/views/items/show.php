db8828044dd6f44b28f6163916b3112a4436916f
Pawan Tharusha <pawantharusha7002@gmail.com>
2026-04-23 10:12:21 +0530

    show.php - item detail view

---
+++ resources/views/items/show.php
@@ -0,0 +1,4 @@
+<?php require_once ROOT . '/resources/views/layouts/header.php'; ?>
+<link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/item-detail.css">
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+    integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
