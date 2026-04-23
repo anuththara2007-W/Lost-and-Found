cae57cb5c33d89313101e0eef4966ef1774e6d74
Pawan Tharusha <pawantharusha7002@gmail.com>
2026-04-23 22:18:13 +0530

    item.php - searchItems updated

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -44,3 +44,54 @@ class Item
         $stmt->execute();
         return $stmt->fetchAll();
     }
+
+    public function searchItems($keyword, $type, $category_id = null, $location = null, $date = null)
+    {
+        $query = "
+            SELECT r.*, u.username, c.name as category_name
+            FROM reports r
+            LEFT JOIN users u ON r.user_id = u.user_id
+            LEFT JOIN categories c ON r.category_id = c.category_id
+            WHERE r.status = 'open'
+        ";
+        $params = [];
+        
+        if (!empty($keyword)) {
+            $query .= " AND (r.title LIKE :keyword_title OR r.description LIKE :keyword_desc OR r.custom_category LIKE :keyword_cat)";
+            $params[':keyword_title'] = '%' . $keyword . '%';
+            $params[':keyword_desc'] = '%' . $keyword . '%';
+            $params[':keyword_cat'] = '%' . $keyword . '%';
+        }
+        
+        if (!empty($type) && in_array($type, ['lost', 'found'])) {
+            $query .= " AND r.type = :type";
+            $params[':type'] = $type;
+        }
+
+        if (!empty($category_id)) {
+            $query .= " AND r.category_id = :category_id";
+            $params[':category_id'] = $category_id;
+        }
+
+        if (!empty($location)) {
+            $query .= " AND r.location LIKE :location";
+            $params[':location'] = '%' . $location . '%';
+        }
+
+        if (!empty($date)) {
+            if ($date === 'today') {
+                $query .= " AND DATE(r.date_posted) = CURDATE()";
+            } elseif ($date === 'week') {
+                $query .= " AND r.date_posted >= DATE_SUB(CURDATE(), INTERVAL 7 DAY)";
+            } elseif ($date === 'month') {
+                $query .= " AND r.date_posted >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH)";
+            }
+        }
+        
+        $query .= " ORDER BY r.date_posted DESC";
+        
+        $stmt = $this->db->prepare($query);
+        $stmt->execute($params);
+        return $stmt->fetchAll();
+    }
+
