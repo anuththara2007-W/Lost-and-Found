6810e006e82daa94a0bb555eedc1e0c43e2c9ede
Pawan Tharusha <pawantharusha7002@gmail.com>
2026-04-24 13:20:39 +0530

    map.php - leafllet CSS updated

--- resources/views/map.php
+++ resources/views/map.php
@@ -11,3 +11,27 @@ foreach ($allItems as $item) {
     }
 }
 ?>
+
+<!-- Leaflet CSS -->
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
+<link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/map.css">
+
+<div class="map-container">
+    <div class="map-header">
+        <div>
+            <h1 class="map-title">Interactive Map</h1>
+            <p class="map-subtitle">
+                Browse reported lost items by their geographical location.
+                <span class="card-badge map-badge map-badge-lost">Lost</span>
+            </p>
+        </div>
+    </div>
+
+    <div class="map-wrapper">
+        <div id="laf-map" class="laf-map-element"></div>
+    </div>
+
+    <?php if (empty($lostItems)): ?>
+        <div class="map-empty-note">No lost items available to show on the map.</div>
+    <?php endif; ?>
+</div>
